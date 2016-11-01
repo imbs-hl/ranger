fa7b24b92cca3c41b5bada17b36bbdc3b3911797
Marvin Wright <wright@imbs.uni-luebeck.de>
2016-11-01 20:51:17 +0100

    add tests for extratrees

---
+++ ranger-r-package/ranger/tests/testthat/test_extratrees.R
@@ -0,0 +1,27 @@
+library(ranger)
+library(survival)
+context("ranger_extratrees")
+
+test_that("extratrees splitting works for classification", {
+  rf <- ranger(Species ~ ., iris, splitrule = "extratrees")
+  expect_is(rf, "ranger")
+  expect_gt(rf$r.squared, 0.5)
+})
+
+test_that("extratrees splitting works for probability estimation", {
+  rf <- ranger(Species ~ ., iris, probability = TRUE, splitrule = "extratrees")
+  expect_is(rf, "ranger")
+  expect_gt(rf$r.squared, 0.5)
+})
+
+test_that("extratrees splitting works for regression", {
+  rf <- ranger(Sepal.Length ~ ., iris, splitrule = "extratrees")
+  expect_is(rf, "ranger")
+  expect_gt(rf$r.squared, 0.5)
+})
+
+test_that("extratrees splitting works for survival", {
+  rf <- ranger(Surv(time, status) ~ ., veteran, splitrule = "extratrees")
+  expect_is(rf, "ranger")
+  expect_lt(rf$prediction.error, 0.4)
+})
