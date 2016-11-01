d51a73e92839642a32203c4cd5dc7658ba92818d
Marvin Wright <wright@imbs.uni-luebeck.de>
2016-11-01 22:28:57 +0100

    add tests for unordered extratrees

--- ranger-r-package/ranger/tests/testthat/test_extratrees.R
+++ ranger-r-package/ranger/tests/testthat/test_extratrees.R
@@ -8,8 +8,20 @@ test_that("extratrees splitting works for classification", {
   expect_gt(rf$r.squared, 0.5)
 })
 
+test_that("extratrees unordered splitting works for classification", {
+  rf <- ranger(Species ~ ., iris, splitrule = "extratrees", respect.unordered.factors = "partition")
+  expect_is(rf, "ranger")
+  expect_gt(rf$r.squared, 0.5)
+})
+
 test_that("extratrees splitting works for probability estimation", {
   rf <- ranger(Species ~ ., iris, probability = TRUE, splitrule = "extratrees")
+  expect_is(rf, "ranger")
+  expect_gt(rf$r.squared, 0.5)
+})
+
+test_that("extratrees unordered splitting works for probability estimation", {
+  rf <- ranger(Species ~ ., iris, probability = TRUE, splitrule = "extratrees", respect.unordered.factors = "partition")
   expect_is(rf, "ranger")
   expect_gt(rf$r.squared, 0.5)
 })
@@ -20,8 +32,20 @@ test_that("extratrees splitting works for regression", {
   expect_gt(rf$r.squared, 0.5)
 })
 
+test_that("extratrees unordered splitting works for regression", {
+  rf <- ranger(Sepal.Length ~ ., iris, splitrule = "extratrees", respect.unordered.factors = "partition")
+  expect_is(rf, "ranger")
+  expect_gt(rf$r.squared, 0.5)
+})
+
 test_that("extratrees splitting works for survival", {
   rf <- ranger(Surv(time, status) ~ ., veteran, splitrule = "extratrees")
+  expect_is(rf, "ranger")
+  expect_lt(rf$prediction.error, 0.4)
+})
+
+test_that("extratrees unordered splitting works for survival", {
+  rf <- ranger(Surv(time, status) ~ ., veteran, splitrule = "extratrees", respect.unordered.factors = "partition")
   expect_is(rf, "ranger")
   expect_lt(rf$prediction.error, 0.4)
 })
