bd5db5e54ccda17080e18f61d4084284247bdead
Marvin Wright <wright@imbs.uni-luebeck.de>
2017-02-03 13:16:53 +0100

    change formula interface to faster version including interactions

--- ranger-r-package/ranger/tests/testthat/test_interface.R
+++ ranger-r-package/ranger/tests/testthat/test_interface.R
@@ -26,3 +26,33 @@ test_that("Interaction included if * in formula", {
   expect_equal(sort(rf$forest$independent.variable.names), 
                sort(c("Petal.Length", "Sepal.Length", "Sepal.Width", "Sepal.Length:Sepal.Width")))
 })
+
+## Formula interface, survival
+test_that("All variables included if . in formula", {
+  rf <- ranger(Surv(time, status) ~ ., veteran, num.trees = 5)
+  expect_equal(sort(rf$forest$independent.variable.names), 
+               sort(colnames(veteran)[c(1:2, 5:8)]))
+})
+
+test_that("Variable excluded if - in formula", {
+  rf <- ranger(Surv(time, status) ~ . - celltype - age, veteran, num.trees = 5)
+  expect_equal(sort(rf$forest$independent.variable.names), 
+               sort(c("trt", "karno", "diagtime", "prior")))
+})
+
+test_that("Interaction included if : in formula", {
+  rf <- ranger(Surv(time, status) ~ celltype + age:prior, veteran, num.trees = 5)
+  expect_equal(sort(rf$forest$independent.variable.names), 
+               sort(c("celltype", "age:prior")))
+})
+
+test_that("Interaction included if * in formula", {
+  rf <- ranger(Surv(time, status) ~ celltype + age*prior, veteran, num.trees = 5)
+  expect_equal(sort(rf$forest$independent.variable.names), 
+               sort(c("celltype", "age", "prior", "age:prior")))
+})
+
+test_that("Error if interaction of factor variable included", {
+  expect_error(ranger(Surv(time, status) ~ celltype*prior, veteran, num.trees = 5), 
+               "Error: Only numeric columns allowed in interaction terms.")
+})
