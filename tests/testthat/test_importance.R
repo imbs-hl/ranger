1a35c96d4f48e69e636edeaf77f2a8ad292eadb5
Marvin Wright <wright@imbs.uni-luebeck.de>
2017-04-20 15:39:53 +0200

    fix survival permutation importance test

--- tests/testthat/test_importance.R
+++ tests/testthat/test_importance.R
@@ -71,11 +71,11 @@ test_that("unscaled importance is smaller than 1", {
   expect_lt(rg.perm.surv$variable.importance[3], 1)
 })
 
-test_that("scaled importance is larger than 1", {
-  expect_gt(rg.scale.perm.class$variable.importance[1], 1)
-  expect_gt(rg.scale.perm.prob$variable.importance[1], 1)
-  expect_gt(rg.scale.perm.regr$variable.importance[1], 1)
-  expect_gt(rg.scale.perm.surv$variable.importance[3], 1)
+test_that("scaled importance is larger than unscaled importance", {
+  expect_gt(abs(rg.scale.perm.class$variable.importance[1]), abs(rg.perm.class$variable.importance[1]))
+  expect_gt(abs(rg.scale.perm.prob$variable.importance[1]), abs(rg.perm.prob$variable.importance[1]))
+  expect_gt(abs(rg.scale.perm.regr$variable.importance[1]), abs(rg.perm.regr$variable.importance[1]))
+  expect_gt(abs(rg.scale.perm.surv$variable.importance[1]), abs(rg.perm.surv$variable.importance[1]))
 })
 
 test_that("error thrown if no importance in object", {
