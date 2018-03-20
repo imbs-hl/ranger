0031ff86d81451ce8fa70f485adb37cbe9b1fb50
Marvin Wright <github@wrig.de>
2018-03-20 14:22:24 +0100

    update multiclass ordering method

--- R/utility.R
+++ R/utility.R
@@ -41,16 +41,21 @@ save.sample <- function(x, ...) {
 # Reference: Coppersmith, D., Hong, S.J. & Hosking, J.R. (1999) Partitioning Nominal Attributes in Decision Trees. Data Min Knowl Discov 3:197. \url{https://doi.org/10.1023/A:1009869804967}.
 pca.order <- function(y, x) {
   x <- droplevels(x)
+  if (nlevels(x) < 2) {
+    return(as.character(levels(x)))
+  }
   
   ## Create contingency table of the nominal outcome with the nominal covariate
-  N <- table(droplevels(y), x)
+  N <- table(x, droplevels(y))
   
-  ## PCA of class probabilites
+  ## PCA of weighted covariance matrix of class probabilites
   P <- N/rowSums(N)
-  pc1 <- prcomp(P, rank. = 1)$rotation
+  S <- cov.wt(P, wt = rowSums(N))$cov
+  pc1 <- prcomp(S, rank. = 1)$rotation
+  score <- P %*% pc1
   
   ## Return ordered factor levels
-  as.character(levels(x)[order(pc1)])
+  as.character(levels(x)[order(score)])
 }
 
 # Compute median survival if available or largest quantile available in all strata if median not available.
