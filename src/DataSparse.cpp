3cb099508374536195ed61d6c5a714b789233de3
Daniel Cooke <dcooke@well.ox.ac.uk>
2018-04-10 20:45:44 +0100

    Correct DataSparse constructor and move into cpp file

--- src/DataSparse.cpp
+++ src/DataSparse.cpp
@@ -29,5 +29,13 @@
 #include "DataSparse.h"
 
 namespace ranger {
+  
+DataSparse::DataSparse(std::shared_ptr<Eigen::SparseMatrix<double>> data, std::vector<std::string> variable_names, size_t num_rows, size_t num_cols)
+: data {data} {
+  this->variable_names = variable_names;
+  this->num_rows = num_rows;
+  this->num_cols = num_cols;
+  this->num_cols_no_snp = num_cols;
+}
 
 } // namespace ranger
