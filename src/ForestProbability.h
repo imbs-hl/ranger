e9b1905ac3581ee886c4ee36d7e9f6afeec8be23
Daniel Cooke <dcooke@well.ox.ac.uk>
2018-04-11 09:05:25 +0100

    Correct spelling error in method name

--- src/ForestProbability.h
+++ src/ForestProbability.h
@@ -71,7 +71,7 @@ protected:
   std::map<std::pair<double, double>, size_t> classification_table;
   
 private:
-  const std::vector<double>& getTreePrediciton(size_t tree_idx, size_t sample_idx) const;
+  const std::vector<double>& getTreePrediction(size_t tree_idx, size_t sample_idx) const;
   size_t getTreePredictionTerminalNodeID(size_t tree_idx, size_t sample_idx) const;
 };
 
