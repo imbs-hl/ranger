b90de56db2931e64080c896aa3449d54272686ca
Marvin Wright <github@wrig.de>
2018-05-28 13:05:35 +0200

    remove GenABEL from suggests and condition tests

--- tests/testthat/test_genabel.R
+++ tests/testthat/test_genabel.R
@@ -3,20 +3,20 @@ library(survival)
 context("genabel")
 
 ## GenABEL
-if (!requireNamespace("GenABEL", quietly = TRUE)) {
-  stop("Package GenABEL is required for testing ranger completely. Please install it.", call. = FALSE)
-} else {
+if (requireNamespace("GenABEL", quietly = TRUE)) {
   library(GenABEL)
   dat.gwaa <- readRDS("../test_gwaa.Rds")
   rg.gwaa <- ranger(CHD ~ ., data = dat.gwaa, verbose = FALSE, write.forest = TRUE)
 }
 
 test_that("classification gwaa rf is of class ranger with 15 elements", {
+  skip_if_not_installed("GenABEL")
   expect_is(rg.gwaa, "ranger")
   expect_equal(length(rg.gwaa), 15)
 })
 
 test_that("GenABEL prediction works if no covariates and formula used", {
+  skip_if_not_installed("GenABEL")
   dat <- dat.gwaa
   dat@phdata$Age <- NULL
   rf <- ranger(CHD ~ .-Sex, data = dat, num.trees = 5)
@@ -24,18 +24,21 @@ test_that("GenABEL prediction works if no covariates and formula used", {
 })
 
 test_that("SNP ordering working for binary classification", {
+  skip_if_not_installed("GenABEL")
   rf <- ranger(CHD ~ ., data = dat.gwaa, num.trees = 5, respect.unordered.factors = "order")
   expect_is(rf$forest$snp.order, "list")
   expect_length(rf$forest$snp.order, nsnps(dat.gwaa))
 })
 
 test_that("SNP ordering working for regression", {
+  skip_if_not_installed("GenABEL")
   rf <- ranger(Age ~ ., data = dat.gwaa, num.trees = 5, respect.unordered.factors = "order")
   expect_is(rf$forest$snp.order, "list")
   expect_length(rf$forest$snp.order, nsnps(dat.gwaa))
 })
 
 test_that("SNP ordering not working for multiclass", {
+  skip_if_not_installed("GenABEL")
   dat <- dat.gwaa
   dat@phdata$mc <- factor(round(runif(nids(dat), 1, 5)))
   expect_error(ranger(mc ~ ., data = dat, num.trees = 5, respect.unordered.factors = "order"), 
@@ -43,6 +46,7 @@ test_that("SNP ordering not working for multiclass", {
 })
 
 test_that("SNP ordering not working for survival", {
+  skip_if_not_installed("GenABEL")
   dat <- dat.gwaa
   dat@phdata$time <- runif(nids(dat), 1, 100)
   dat@phdata$status <- rbinom(nids(dat), 1, 0.5)
@@ -51,6 +55,7 @@ test_that("SNP ordering not working for survival", {
 })
 
 test_that("SNP ordering working with corrected importance", {
+  skip_if_not_installed("GenABEL")
   rf <- ranger(CHD ~ ., data = dat.gwaa, num.trees = 5, respect.unordered.factors = "order", 
                importance = "impurity_corrected")
   expect_is(rf$forest$snp.order, "list")
@@ -63,6 +68,7 @@ test_that("SNP ordering working with corrected importance", {
 })
 
 test_that("SNP ordering not working with corrected importance for survival", {
+  skip_if_not_installed("GenABEL")
   dat <- dat.gwaa
   dat@phdata$time <- runif(nids(dat), 1, 100)
   dat@phdata$status <- rbinom(nids(dat), 1, 0.5)
