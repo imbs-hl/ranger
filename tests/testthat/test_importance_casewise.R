1834778f84f7ad7128093237d1340611b6143cfd
Robrecht Cannoodt <rcannood@gmail.com>
2019-10-06 14:37:42 +0200

    add unit test for casewise variable importances

---
+++ tests/testthat/test_importance_casewise.R
@@ -0,0 +1,97 @@
+context("test_casewise_importances")
+
+test_that("casewise importance works, classification", {
+  data <- 
+    data.frame(
+      x = runif(1000),
+      y = rnorm(1000, mean = 1),
+      z = rnorm(1000, mean = 2)
+    )
+  rownames(data) <- paste0("case_", seq_len(nrow(data)))
+  data$a <- factor(ifelse(ifelse(data$x < .5, data$y, data$z) > 1.5, "left", "right"))
+  
+  rf <- ranger(
+    data = data,
+    dependent.variable.name = "a",
+    importance = "permutation",
+    local.importance = TRUE,
+    num.threads = 1
+  )
+  vic <- rf$variable.importance.casewise
+  
+  # should see clear pattern here:
+  # pheatmap::pheatmap(vic[order(data$x),], cluster_cols = FALSE, cluster_rows = FALSE)
+  
+  expect_equal(rownames(vic), rownames(data))
+  expect_equal(colnames(vic), colnames(data)[1:3])
+  
+  expect_lte(wilcox.test(vic[data$x < .5, 2], vic[data$x >= .5, 2], "greater")$p.value, .01)
+  expect_gte(wilcox.test(vic[data$x < .5, 2], vic[data$x >= .5, 2], "less")$p.value, .99)
+  expect_lte(wilcox.test(vic[data$x < .5, 3], vic[data$x >= .5, 3], "less")$p.value, .01)
+  expect_gte(wilcox.test(vic[data$x < .5, 3], vic[data$x >= .5, 3], "greater")$p.value, .99)
+})
+
+test_that("casewise importance works, regression", {
+  data <- 
+    data.frame(
+      x = runif(1000),
+      y = rnorm(1000, mean = 1),
+      z = rnorm(1000, mean = 2)
+    )
+  rownames(data) <- paste0("case_", seq_len(nrow(data)))
+  data$a <- ifelse(data$x < .5, data$y, data$z)
+  
+  rf <- ranger(
+    data = data,
+    dependent.variable.name = "a",
+    importance = "permutation",
+    local.importance = TRUE,
+    num.threads = 1
+  )
+  vic <- rf$variable.importance.casewise
+  
+  # should see clear pattern here:
+  # pheatmap::pheatmap(vic[order(data$x),], cluster_cols = FALSE, cluster_rows = FALSE)
+  
+  expect_equal(rownames(vic), rownames(data))
+  expect_equal(colnames(vic), colnames(data)[1:3])
+  
+  expect_lte(wilcox.test(vic[data$x < .5, 2], vic[data$x >= .5, 2], "greater")$p.value, .01)
+  expect_gte(wilcox.test(vic[data$x < .5, 2], vic[data$x >= .5, 2], "less")$p.value, .99)
+  expect_lte(wilcox.test(vic[data$x < .5, 3], vic[data$x >= .5, 3], "less")$p.value, .01)
+  expect_gte(wilcox.test(vic[data$x < .5, 3], vic[data$x >= .5, 3], "greater")$p.value, .99)
+})
+
+
+test_that("casewise importance works, probability", {
+  data <- 
+    data.frame(
+      x = runif(1000),
+      y = rnorm(1000, mean = 1),
+      z = rnorm(1000, mean = 2)
+    )
+  rownames(data) <- paste0("case_", seq_len(nrow(data)))
+  # data$a <- ifelse(data$x < .5, data$y, data$z)
+  data$a <- factor(ifelse(ifelse(data$x < .5, data$y, data$z) > 1.5, "left", "right"))
+  
+  rf <- ranger(
+    data = data,
+    dependent.variable.name = "a",
+    importance = "permutation",
+    probability = TRUE,
+    local.importance = TRUE,
+    num.threads = 1
+  )
+  vic <- rf$variable.importance.casewise
+  
+  # should see clear pattern here:
+  # pheatmap::pheatmap(vic[order(data$x),], cluster_cols = FALSE, cluster_rows = FALSE)
+  
+  expect_equal(rownames(vic), rownames(data))
+  expect_equal(colnames(vic), colnames(data)[1:3])
+  
+  expect_lte(wilcox.test(vic[data$x < .5, 2], vic[data$x >= .5, 2], "greater")$p.value, .01)
+  expect_gte(wilcox.test(vic[data$x < .5, 2], vic[data$x >= .5, 2], "less")$p.value, .99)
+  expect_lte(wilcox.test(vic[data$x < .5, 3], vic[data$x >= .5, 3], "less")$p.value, .01)
+  expect_gte(wilcox.test(vic[data$x < .5, 3], vic[data$x >= .5, 3], "greater")$p.value, .99)
+})
