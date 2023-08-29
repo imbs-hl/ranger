9df703f49950b9f567d00d3e588771f5f203a856
Marvin Wright <github@wrig.de>
2023-08-29 08:00:59 +0200

    try other values from new R version

--- tests/testthat/test_reproducibility.R
+++ tests/testthat/test_reproducibility.R
@@ -8,7 +8,7 @@ test_that("returns same predictions as before, R seed", {
   rf <- ranger(Species ~ ., iris, probability = TRUE)
   pred <- predict(rf, iris)
   
-  expect_equal(pred$predictions[148:150, 3], c(1.0, 0.9871778, 0.9511746), tolerance = 1e-5)
+  expect_equal(pred$predictions[148:150, 3], c(1.0, 0.9940865, 0.9197802), tolerance = 1e-5)
 })
 
 test_that("returns same predictions as before, cpp seed", {
@@ -16,7 +16,7 @@ test_that("returns same predictions as before, cpp seed", {
   rf <- ranger(Species ~ ., iris, probability = TRUE, seed = 2023)
   pred <- predict(rf, iris, seed = 2023)
   
-  expect_equal(pred$predictions[148:150, 3], c(0.9997500, 0.9889944, 0.9401063), tolerance = 1e-5)
+  expect_equal(pred$predictions[148:150, 3], c(1.0, 0.9919810, 0.9377452), tolerance = 1e-5)
 })
 
 
