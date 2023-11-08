18a193cffda40b8452b5796b7144899c0ad01c32
Marvin Wright <github@wrig.de>
2023-11-08 09:26:36 +0100

    C++14 not C++11

--- src/AAA_check_cpp11.cpp
+++
@@ -1,6 +0,0 @@
-#ifndef WIN_R_BUILD
-#if __cplusplus < 201402L
-#error Error: ranger requires C++14. Possible fixes: 1) Update R, 2) Set "CXX = g++ -std=gnu++11" or similar in local Makevars, 3) update C++ compiler. See https://github.com/imbs-hl/ranger/wiki/FAQ. 
-#endif
-#endif
-
