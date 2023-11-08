ccce3ab27729b0c1be2be06bcc4ade8ca156c22b
Marvin Wright <github@wrig.de>
2023-11-08 07:11:09 +0100

    improve C++14 error #669

--- src/AAA_check_cpp11.cpp
+++ src/AAA_check_cpp11.cpp
@@ -1,6 +1,6 @@
 #ifndef WIN_R_BUILD
 #if __cplusplus < 201402L
-#error Error: ranger requires a C++14 compiler, e.g., gcc >= 5 or Clang >= 3.4. You probably have to update your C++ compiler.
+#error Error: ranger requires C++14. Possible fixes: 1) Update R, 2) Set "CXX = g++ -std=gnu++11" or similar in local Makevars, 3) update C++ compiler. See https://github.com/imbs-hl/ranger/wiki/FAQ. 
 #endif
 #endif
 
