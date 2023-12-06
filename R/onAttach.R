33ea5da1d3bbe2d20a187c67ffad7d08b270f90d
Marvin Wright <github@wrig.de>
2023-12-06 21:14:11 +0100

    also use Ncpus option

--- R/onAttach.R
+++ R/onAttach.R
@@ -4,12 +4,16 @@
     return()
   }
   
-  threads_option <- getOption("ranger.num.threads")
   threads_env <- Sys.getenv("R_RANGER_NUM_THREADS")
-  if (!is.null(threads_option)) {
-    thread_string <- paste(threads_option, "threads as set by options(ranger.num.threads = N). Can be overwritten with num.threads.")
-  } else if (threads_env != "") {
+  threads_option1 <- getOption("ranger.num.threads")
+  threads_option2 <- getOption("Ncpus")
+  
+  if (threads_env != "") {
     thread_string <- paste(threads_env, "threads as set by environment variable R_RANGER_NUM_THREADS. Can be overwritten with num.threads.")
+  } else if (!is.null(threads_option1)) {
+    thread_string <- paste(threads_option1, "threads as set by options(ranger.num.threads = N). Can be overwritten with num.threads.")
+  } else if (!is.null(threads_option2)) {
+    thread_string <- paste(threads_option2, "threads as set by options(Ncpus = N). Can be overwritten with num.threads.")
   } else {
     thread_string <- "2 threads (default). Change with num.threads in ranger() and predict(), options(ranger.num.threads = N) or environment variable R_RANGER_NUM_THREADS."
   }
